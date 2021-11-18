d452790209f3647f05aeb1572a3de05ef88a7bc7
Utku <utku.karatas3@gmail.com>
2021-11-18 18:09:41 +0300

    updated 18.11.21--6.09pm

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -17,6 +17,7 @@ class MainActivity : AppCompatActivity() {
         rollButton.setOnClickListener {
             val toast = Toast.makeText(this,"Dice Rolled!", Toast.LENGTH_SHORT).show()
             rollDice()
+            main()
         }
         //main()
     }
@@ -44,4 +45,38 @@ class Dice(val numSides: Int){
     fun roll(): Int{
         return(1..numSides).random()
     }
+}
+
+fun main() {
+    val myFirstDice = Dice(6)
+    val rollResult = myFirstDice.roll()
+    val luckyNumber = 4
+
+    when(rollResult){
+        luckyNumber -> println("You Win!")
+        1 -> println("So sorry! You rolled a 1. Try again!")
+        2 -> println("Sadly, you rolled a 2. Try again!")
+        3 -> println("Unfortunately, you rolled a 3. Try again!")
+        5 -> println("Don't cry! You rolled a 5. Try again!")
+        6 -> println("Apologies! you rolled a 6. Try again!")
+    }
+
+
+    /*
+    if (rollResult == luckyNumber) {
+        println("You win!")
+    } else if (rollResult == 1) {
+        println("So sorry! You rolled a 1. Try again!")
+    } else if (rollResult == 2) {
+        println("Sadly, you rolled a 2. Try again!")
+    } else if (rollResult == 3) {
+        println("Unfortunately, you rolled a 3. Try again!")
+    } else if (rollResult == 5) {
+        println("Don't cry! You rolled a 5. Try again!")
+    } else {
+        println("Apologies! You rolled a 6. Try again!")
+    }
+    println("Your ${myFirstDice.numSides} sided dice rolled ${rollResult}!")
+
+     */
 }
